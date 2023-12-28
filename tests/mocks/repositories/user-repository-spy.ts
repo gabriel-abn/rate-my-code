719c560e7ee48f880320e5b823815b39f230c149
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-12-28 11:37:38 -0300

    feat: created user repository spy class

---
+++ tests/mocks/repositories/user-repository-spy.ts
@@ -0,0 +1,48 @@
+import IUserRepository from "@application/repositories/user-repository";
+import { CheckEmailAvailability } from "@application/use-cases/sign-in-use-case";
+import { User } from "@domain/entities";
+
+export default class UserRepositorySpy
+  implements IUserRepository, CheckEmailAvailability
+{
+  users: User[];
+
+  constructor() {
+    this.users = [
+      new User(
+        {
+          email: "fake@gmail.com",
+          password: "fake",
+        },
+        "fake",
+      ),
+    ];
+  }
+
+  async checkEmail(email: string): Promise<boolean> {
+    const user = this.users.find((user) => user.email === email);
+
+    if (user) {
+      return true;
+    }
+
+    return false;
+  }
+
+  async save(user: User): Promise<string> {
+    this.users.push(user);
+
+    return user.id;
+  }
+
+  async getById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+  async update(id: string, user: User): Promise<User> {
+    const index = this.users.findIndex((user) => user.id === id);
+
+    this.users[index] = user;
+
+    return user;
+  }
+}

b2298c072e4aa0b866ab2a09fca9ae3a454137ec
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-12-28 11:45:56 -0300

    feat(SignIn): :sparkles: implemented 'save user' and 'save confirmation token' method to use case
    Passing unit tests.

--- src/application/use-cases/sign-in-use-case.ts
+++ src/application/use-cases/sign-in-use-case.ts
@@ -1,7 +1,10 @@
 import ApplicationError from "@application/common/application-error";
 import { IEncrypter, IHasher } from "@application/protocols";
+import SendEmail from "@application/protocols/services/send-email";
 import IUserRepository from "@application/repositories/user-repository";
+import { User } from "@domain/entities";
 import SignIn from "@domain/use-cases/sign-in";
+import { randomUUID } from "crypto";
 
 export interface CheckEmailAvailability {
   checkEmail(email: string): Promise<boolean>;
@@ -15,7 +18,9 @@ class SignInUseCase implements SignIn.UseCase {
   constructor(
     private readonly userRepo: IUserRepository & CheckEmailAvailability,
     private readonly hasher: IHasher,
-    private readonly tokenGenerator: IEncrypter,
+    private readonly crypter: IEncrypter,
+    private readonly saveToken: SaveVerificationToken,
+    private readonly sendEmail: SendEmail.Service,
   ) {}
 
   async execute(data: SignIn.Params): Promise<SignIn.Result> {
@@ -25,12 +30,35 @@ class SignInUseCase implements SignIn.UseCase {
       throw new ApplicationError("Email already in use.", "EMAIL_IN_USE");
     }
 
+    const confirmationToken = randomUUID().split("-")[0].toUpperCase();
+
+    const emailSent = await this.sendEmail.send({
+      to: data.email,
+      template: "email-confirmation",
+      data: {
+        token: confirmationToken,
+      },
+    });
+
+    if (!emailSent) {
+      throw new ApplicationError("Error sending email.", "EMAIL_SEND_ERROR");
+    }
+
+    await this.saveToken.saveToken(data.email, confirmationToken);
+
     const hashedPassword = await this.hasher.hash(data.password);
 
-    const accessToken = await this.tokenGenerator.encrypt({
-      email: data.email,
-      password: hashedPassword,
-    });
+    const user = new User(
+      {
+        email: data.email,
+        password: hashedPassword,
+      },
+      "fake",
+    );
+
+    const savedUser = await this.userRepo.save(user);
+
+    const accessToken = await this.crypter.encrypt(savedUser);
 
     return {
       accessToken,

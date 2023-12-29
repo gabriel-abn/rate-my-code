e72834e397bb318fc7e2e5527ff2ce63eac328f2
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-12-29 20:59:42 -0300

    feat: :art: created database common error class

---
+++ src/infra/persistence/common/database-error.ts
@@ -0,0 +1,7 @@
+import ApplicationError from "@application/common/application-error";
+
+export class DatabaseError extends ApplicationError {
+  constructor(message: string, error?: Error) {
+    super(message, error.name);
+  }
+}
--- src/infra/persistence/common/index.ts
+++ src/infra/persistence/common/index.ts
@@ -1,3 +1,4 @@
+export * from "./database-error";
 export * from "./key-value-database";
 export * from "./no-relational-database";
 export * from "./relational-database";

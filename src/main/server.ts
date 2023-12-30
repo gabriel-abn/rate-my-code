49b1cf2b52ce0d066acaa585e37b12e62906c75f
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-12-29 21:05:44 -0300

    feat: :sparkles: using single instance database repositories

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,27 @@
 import SignInUseCase from "@application/use-cases/sign-in-use-case";
+
 import Hasher from "@infra/cryptography/hasher";
 import JWTAdapter from "@infra/jwt/jwt-crypter";
-import UserRepository from "@infra/repositories/user-repository";
+import tokenRepository from "@infra/persistence/repositories/token-repository";
+import userRepository from "@infra/persistence/repositories/user-repository";
+import emailService from "@infra/services/email-service";
+
 import SignInController from "@presentation/controllers/sign-in-controller";
+
 import express, { Request, Response, json } from "express";
+import loadEnvVars from "./config/env";
+
+loadEnvVars();
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.use(json());
+
+app.listen(PORT, () => {
+  console.log(`Environment: ${process.env.NODE_ENV}\n`);
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
@@ -17,16 +30,19 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/api/sign-in", async (req: Request, res: Response) => {
   const body = req.body;
 
-  const useCase = new SignInUseCase(new UserRepository(), new Hasher(), new JWTAdapter());
+  const useCase = new SignInUseCase(
+    userRepository,
+    new Hasher(),
+    new JWTAdapter(),
+    tokenRepository,
+    emailService,
+  );
+
   const controller = new SignInController(useCase);
 
   const response = await controller.handle({ body });
 
   res.status(response.statusCode).send({ ...response.body });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
 
 export default app;

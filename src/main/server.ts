7bf9358854dc0ffa8fcdafaf33c33641a3abf8d1
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-01-02 12:02:08 -0300

    chore: :sparkles: dynamic port assignment to express app

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,7 +6,7 @@ import express, { Request, Response, json } from "express";
 loadEnvVars();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(json());
 
--- tests/config/vitest.integration.config.ts
+++ tests/config/vitest.integration.config.ts
@@ -6,6 +6,7 @@ export default mergeConfig(baseConfig, {
     include: ["**/*.test.ts"],
     env: {
       NODE_ENV: "development",
+      PORT: 0,
     },
   },
 });

0de60b49bb39d943811cbb913efff4b0093add85
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-01-02 15:04:37 -0300

    feat(VerifyEmail): :sparkles: implemented feature factory and route

---
+++ src/main/factories/account/verify-email-factory.ts
@@ -0,0 +1,20 @@
+import { VerifyEmailUseCase } from "@application/use-cases";
+import { VerifyEmail } from "@domain/use-cases";
+import tokenRepository from "@infra/persistence/repositories/token-repository";
+import userRepository from "@infra/persistence/repositories/user-repository";
+import { VerifyEmailController } from "@presentation/controllers/account/verify-email-controller";
+import { Factory } from "../factory";
+
+class VerifyEmailFactory implements Factory {
+  useCase: VerifyEmail.UseCase;
+  controller: VerifyEmailController;
+
+  create(): VerifyEmailController {
+    this.useCase = new VerifyEmailUseCase(userRepository, tokenRepository);
+    this.controller = new VerifyEmailController(this.useCase);
+
+    return this.controller;
+  }
+}
+
+export default new VerifyEmailFactory();
--- src/main/routes/account.ts
+++ src/main/routes/account.ts
@@ -1,8 +1,11 @@
 import { adaptRoute } from "@main/adapters/express-route-adapter";
 import signInFactory from "@main/factories/account/sign-in-factory";
+import verifyEmailFactory from "@main/factories/account/verify-email-factory";
 
 import { Router } from "express";
 
 export const accountRoutes = Router();
 
 accountRoutes.post("/sign-in", adaptRoute(signInFactory.create()));
+
+accountRoutes.post("/verify-email", adaptRoute(verifyEmailFactory.create()));

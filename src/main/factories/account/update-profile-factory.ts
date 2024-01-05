b03b049ead3779d8f64e374f58634ad11d48dd8f
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-01-05 10:04:31 -0300

    feat(UpdateProfile): :sparkles: created controller factory

---
+++ src/main/factories/account/update-profile-factory.ts
@@ -0,0 +1,21 @@
+import { UpdateProfileUseCase } from "@application/use-cases";
+import { UpdateProfile } from "@domain/use-cases";
+import userRepository from "@infra/persistence/repositories/user-repository";
+import Controller from "@presentation/common/controller";
+import { UpdateProfileController } from "@presentation/controllers/account/update-profile-controller";
+import { Factory } from "../factory";
+
+class UpdateProfileFactory implements Factory {
+  useCase: UpdateProfile.UseCase;
+  controller: UpdateProfileController;
+
+  create(): Controller<any> {
+    this.useCase = new UpdateProfileUseCase(userRepository);
+
+    this.controller = new UpdateProfileController(this.useCase);
+
+    return this.controller;
+  }
+}
+
+export default new UpdateProfileFactory();

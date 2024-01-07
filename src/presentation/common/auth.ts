749b967e332efc97e9c280f020080d3acf921392
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-01-06 21:34:36 -0300

    feat: :construction: working on roles and permissions middleware

--- src/presentation/common/auth.ts
+++ src/presentation/common/auth.ts
@@ -1,6 +1,18 @@
-export enum Permission {
-  ADMIN = 0,
-  MANAGER = 1,
-  INSTRUCTOR = 2,
-  DEVELOPER = 3,
-}
+export const permissions = (
+  level: number | "ADMIN" | "MANAGER" | "INSTRUCTOR" | "DEVELOPER",
+): [number, string] => {
+  switch (level) {
+    case 0 || "ADMIN":
+      return [0, "ADMIN"];
+    case 1 || "MANAGER":
+      return [1, "MANAGER"];
+    case 2 || "INSTRUCTOR":
+      return [2, "INSTRUCTOR"];
+    case 3 || "DEVELOPER":
+      return [3, "DEVELOPER"];
+    default:
+      throw new Error("Invalid permission level");
+  }
+};
+
+export type Payload = Record<string, { email: string; permission: [number, string] }>;
